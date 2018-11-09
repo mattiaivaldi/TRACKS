89766406148d187cfe542b6d4ab6cc23ba241fd9
Mattia Ivaldi <mattia.ivaldi@gmail.com>
2018-11-09 09:56:08 +0100

    test commit

--- Event.h
+++ Event.h
@@ -5,6 +5,7 @@
 
 #include "TObject.h"
 #include "Hit.h"
+#include "TString.h"
 
 class Event : public Hit {
 
@@ -13,25 +14,25 @@ public:
 	Event(); //default constructor
 	Event(double meanv, double sigmaxy, double sigmaz, const char *distr); //custom constructor
 	//meanv is the mean of the vertex coordinates which is 0 for x, y and z, sigmaxy and sigmaz are the RMS of x, y and z
-	//distr is the name of the file in which the pseudorapidity distribution is located  
-	virtual ~Event(); //destructor 
+	//distr is the name of the file in which the pseudorapidity distribution is located
+	virtual ~Event(); //destructor
 
 //member functions
 
-	double GetX() const; //getters 
+	double GetX() const; //getters
 	double GetY() const;
 	double GetZ() const;
-	float GetMult() const; 
+	float GetMult() const;
 
 private:
 
-//data members 	
+//data members
 
 	double fX0; //primary vertex cooridnates
 	double fY0;
-	double fZ0;	
-	float fMult; //multiplicity 
-	
+	double fZ0;
+	float fMult; //multiplicity
+
 	ClassDef(Event,1)
 
 };
