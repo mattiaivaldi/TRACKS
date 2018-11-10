52b4fad18cacce4db523e7da07025461d914f67a
Mattia Ivaldi <mattia.ivaldi@gmail.com>
2018-11-11 00:39:43 +0100

    add

---
+++ Event_cxx.d
@@ -0,0 +1,74 @@
+
+# DO NOT DELETE
+
+Event_cxx.so: /Applications/root_v6.14.04/include/TRandom2.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TRandom.h
+Event_cxx.so: /Applications/root_v6.14.04/include/Math/TRandomEngine.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TNamed.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TObject.h
+Event_cxx.so: /Applications/root_v6.14.04/include/RConfigure.h
+Event_cxx.so: /Applications/root_v6.14.04/include/Rtypes.h
+Event_cxx.so: /Applications/root_v6.14.04/include/RtypesCore.h
+Event_cxx.so: /Applications/root_v6.14.04/include/ROOT/RConfig.h
+Event_cxx.so: /Applications/root_v6.14.04/include/RVersion.h
+Event_cxx.so: /usr/include/AvailabilityMacros.h
+Event_cxx.so: /usr/include/TargetConditionals.h /usr/include/stddef.h
+Event_cxx.so: /Applications/root_v6.14.04/include/DllImport.h
+Event_cxx.so: /Applications/root_v6.14.04/include/snprintf.h
+Event_cxx.so: /usr/include/stdio.h
+Event_cxx.so: /Applications/root_v6.14.04/include/strlcpy.h
+Event_cxx.so: /usr/include/string.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TGenericClassInfo.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TSchemaHelper.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TIsAProxy.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TVirtualIsAProxy.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TStorage.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TVersionCheck.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TString.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TMathBase.h
+Event_cxx.so: /Applications/root_v6.14.04/include/ROOT/RStringView.hxx
+Event_cxx.so: /Applications/root_v6.14.04/include/ROOT/TypeTraits.hxx
+Event_cxx.so: /Applications/root_v6.14.04/include/ROOT/RSpan.hxx
+Event_cxx.so: /Applications/root_v6.14.04/include/ROOT/span.hxx
+Event_cxx.so: /Applications/root_v6.14.04/include/TRandom3.h
+Event_cxx.so: /Applications/root_v6.14.04/include/Riostream.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TFile.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TDirectoryFile.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TDirectory.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TBuffer.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TList.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TSeqCollection.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TCollection.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TIterator.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TVirtualRWMutex.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TVirtualMutex.h
+Event_cxx.so: /usr/include/assert.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TDatime.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TUUID.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TMap.h
+Event_cxx.so: /Applications/root_v6.14.04/include/THashTable.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TUrl.h
+Event_cxx.so: /Applications/root_v6.14.04/include/ROOT/TRWSpinLock.hxx
+Event_cxx.so: /Applications/root_v6.14.04/include/ROOT/TSpinMutex.hxx
+Event_cxx.so: /Applications/root_v6.14.04/include/ROOT/RConcurrentHashColl.hxx
+Event_cxx.so: /Applications/root_v6.14.04/include/TH1F.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TH1.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TAxis.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TAttAxis.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TArrayD.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TArray.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TAttLine.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TAttFill.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TAttMarker.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TArrayC.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TArrayS.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TArrayI.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TArrayF.h
+Event_cxx.so: /Applications/root_v6.14.04/include/Foption.h
+Event_cxx.so: /Applications/root_v6.14.04/include/Fit/FitExecutionPolicy.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TVectorFfwd.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TVectorDfwd.h
+Event_cxx.so: /Applications/root_v6.14.04/include/TFitResultPtr.h
+Event_cxx.so: /usr/include/float.h Hit.h Event.h
+Event_cxx.so: /Applications/root_v6.14.04/include/RVersion.h /Applications/root_v6.14.04/include/RConfig.h /Applications/root_v6.14.04/include/TClass.h /Applications/root_v6.14.04/include/TDictAttributeMap.h /Applications/root_v6.14.04/include/TInterpreter.h /Applications/root_v6.14.04/include/TROOT.h /Applications/root_v6.14.04/include/TBuffer.h /Applications/root_v6.14.04/include/TMemberInspector.h /Applications/root_v6.14.04/include/TError.h /Applications/root_v6.14.04/include/RtypesImp.h /Applications/root_v6.14.04/include/TIsAProxy.h /Applications/root_v6.14.04/include/TFileMergeInfo.h /Applications/root_v6.14.04/include/TCollectionProxyInfo.h /Applications/root_v6.14.04/bin/rootcling
+Event_cxx__ROOTBUILDVERSION= 6.14/04
--- classcompiler.C
+++ classcompiler.C
@@ -1,4 +1,4 @@
-//Macro used in order to compile all classes. You have to run this macro with .x before executing the main code 
+//Macro used in order to compile all classes. You have to run this macro with .x before executing the main code
 
 void classcompiler(TString myopt="fast"){
     TString opt;
@@ -9,8 +9,8 @@ void classcompiler(TString myopt="fast"){
         opt = "kg";
     }
     gSystem->CompileMacro("Layer.cxx",opt.Data());
-    gSystem->CompileMacro("Tools.cxx",opt.Data());
     gSystem->CompileMacro("Hit.cxx",opt.Data());
     gSystem->CompileMacro("Event.cxx",opt.Data());
     gSystem->CompileMacro("Particle.cxx",opt.Data());
+    gSystem->CompileMacro("Tools.cxx",opt.Data());
 }
