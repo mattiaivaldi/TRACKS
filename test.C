2d4f726b506aa6efeba7868c214d0661394e9075
Mattia Ivaldi <mattia.ivaldi@gmail.com>
2018-11-11 01:14:13 +0100

    edit

--- test.C
+++ test.C
@@ -67,11 +67,11 @@ void test(bool PrintParticles, bool multiscatman) {
       printf("Particle %i: theta %f - phi %f\n",i,theta[i],phi[i]);
     }
 
-    detect(*vgen, theta[i], phi[i], BP, part, cross_BP, PrintParticles, multiscatman);
+    detect(*vgen, theta[i], phi[i], *BP, *part, cross_BP, PrintParticles, multiscatman);
 
-    detect(*vgen, theta[i], phi[i], L1, part, cross_L1, PrintParticles, multiscatman);
+    detect(*vgen, theta[i], phi[i], *L1, *part, cross_L1, PrintParticles, multiscatman);
 
-    detect(*vgen, theta[i], phi[i], L2, part, cross_L2, PrintParticles, multiscatman);
+    detect(*vgen, theta[i], phi[i], *L2, *part, cross_L2, PrintParticles, multiscatman);
 
     delete part;
 
