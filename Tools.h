bca6c9e362dbd087f187c7e3ebefe8cdae1cd1d0
Mattia Ivaldi <mattia.ivaldi@gmail.com>
2018-11-12 10:30:19 +0100

    edit

--- Tools.h
+++ Tools.h
@@ -13,6 +13,6 @@
 //x0, y0, z0 = generated vertex coordinates; theta, phi = generated angles; R = radius of the detector
 double *hit_point(double x0, double y0, double z0, double theta, double phi, double R);
 
-void detect(Event vtx, Layer L, Particle &part, vector<Hit*> &cross, bool b_verbose, bool b_multiscatter, char const *detector);
+void detect(Event vtx, Layer L, Particle part, vector<Hit*> &cross, bool b_verbose, bool b_multiscatter, char const *detector);
 
 #endif
