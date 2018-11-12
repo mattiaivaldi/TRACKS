34d2c3dfbfd55f551aa63b8bb66d87e51574c4fb
Mattia Ivaldi <mattia.ivaldi@gmail.com>
2018-11-12 10:18:03 +0100

    edit

--- Tools.cxx
+++ Tools.cxx
@@ -7,15 +7,15 @@
 #include "Particle.h"
 #include "TMath.h"
 
-using namespace TMath;
+//using namespace TMath;
 
 double *hit_point(double x0, double y0, double z0, double theta, double phi, double R) {
 
     static double hit[3];
-    double c1 = Sin(theta)*Cos(phi), c2 = Sin(theta)*Sin(phi), c3 = Cos(theta); //direction cosines
+    double c1 = TMath::Sin(theta)*TMath::Cos(phi), c2 = TMath::Sin(theta)*TMath::Sin(phi), c3 = TMath::Cos(theta); //direction cosines
     double delta = 2*x0*y0*c1*c2 - c1*c1*y0*y0 + c1*c1*R*R -c2*c2*x0*x0 + c2*c2*R*R; //delta of II degree equation ( >= 0 by construction)
-    double t_p = (-(x0*c1 - y0*c2) + Sqrt(delta))/(c1*c1 + c2*c2); //solution with the "+" sign
-    double t_m = (-(x0*c1 - y0*c2) - Sqrt(delta))/(c1*c1 + c2*c2); //solution with the "-" sign
+    double t_p = (-(x0*c1 - y0*c2) + TMath::Sqrt(delta))/(c1*c1 + c2*c2); //solution with the "+" sign
+    double t_m = (-(x0*c1 - y0*c2) - TMath::Sqrt(delta))/(c1*c1 + c2*c2); //solution with the "-" sign
 
     //calculate the values of the intersection points (x,y,z)
     if (t_p >= 0) {
@@ -34,14 +34,15 @@ double *hit_point(double x0, double y0, double z0, double theta, double phi, dou
     return hit;
 }
 
-void detect(Event vtx, double &theta, double &phi, Layer L, Particle part, vector<Hit> cross, b_verbose, bool b_multiscatter){
+void detect(Event vtx, Layer L, Particle part, vector<Hit*> &cross, bool b_verbose, bool b_multiscatter, char const *detector){
 
   double *hit_buffer;
   bool b_cross=false;
+  //double rms=L.GetRMS();
 
-  hit_buffer=hit_point(vtx->GetX(),vtx->GetY(),vtx->GetZ(),theta,phi,L->GetRadius());
+  hit_buffer=hit_point(vtx.GetX(),vtx.GetY(),vtx.GetZ(),part.GetTheta(),part.GetPhi(),L.GetRadius());
 
-  if(*(hit_buffer+2) >= -(L->GetWidth()/2.) && *(hit_buffer+2) <= (L->GetWidth()/2.)) {
+  if(*(hit_buffer+2) >= -(L.GetWidth()/2.) && *(hit_buffer+2) <= (L.GetWidth()/2.)) {
 
     b_cross = true;
 
@@ -49,24 +50,18 @@ void detect(Event vtx, double &theta, double &phi, Layer L, Particle part, vecto
 
     cross.push_back(hit);
 
-    /*if (b_verbose==true) {
-      printf("Hit with BP at (%f, %f, %f)\n",cross[j]->GetX(),cross[j]->GetY(),cross[j]->GetZ());
-    }*/
-
-    cout << "Before beam pipe, phi = " << phi << " theta = " << theta << endl;
-
-    if (b_cross == true && b_multiscatter == true) {
-      part->Rotate(L->GetRMS());
-      phi = part->GetPhi();
-      theta = part->GetTheta();
+    if (b_verbose==true) {
+      printf("Hit with %s at (%f, %f, %f)\n",detector,*(hit_buffer+0),*(hit_buffer+0),*(hit_buffer+0));
     }
 
-    //j++;
+    if (b_cross == true && b_multiscatter == true) {
+      part.Rotate(0.001);
+    }
 
-    //cout << "After beam pipe, phi = " << phi << " theta = " << theta << endl;
+    if (b_multiscatter == true) {
+    printf("Angles after multiple scattering: theta %f - phi %f\n\n",part.GetTheta(),part.GetPhi());
+    }
 
   }
-
-  //return cross;
 
 }
