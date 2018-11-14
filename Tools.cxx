281a2b2e3b8085f714bd316089e769b9afe82236
Mattia Ivaldi <mattia.ivaldi@gmail.com>
2018-11-14 17:23:49 +0100

    edit

--- Tools.cxx
+++ Tools.cxx
@@ -31,10 +31,10 @@ void verbosities(bool b_verbose, bool b_multiscatter, bool b_noise){
 double *hit_point(double x0, double y0, double z0, double theta, double phi, double R) {
 
   static double hit[3];
-  double c1 = TMath::Sin(theta)*TMath::Cos(phi), c2 = TMath::Sin(theta)*TMath::Sin(phi), c3 = TMath::Cos(theta); //direction cosines
-  double delta = 2*x0*y0*c1*c2 - c1*c1*y0*y0 + c1*c1*R*R -c2*c2*x0*x0 + c2*c2*R*R; //delta of II degree equation ( >= 0 by construction)
-  double t_p = (-(x0*c1 - y0*c2) + TMath::Sqrt(delta))/(c1*c1 + c2*c2); //solution with the "+" sign
-  double t_m = (-(x0*c1 - y0*c2) - TMath::Sqrt(delta))/(c1*c1 + c2*c2); //solution with the "-" sign
+  double c1 = TMath::Sin(theta)*TMath::Cos(phi), c2 = TMath::Sin(theta)*TMath::Sin(phi), c3 = TMath::Cos(theta);//direction cosines
+  double delta = 2*x0*y0*c1*c2 - c1*c1*y0*y0 + c1*c1*R*R -c2*c2*x0*x0 + c2*c2*R*R;//delta of II degree equation (>= 0 by construction)
+  double t_p = (-(x0*c1 - y0*c2) + TMath::Sqrt(delta))/(c1*c1 + c2*c2);//+ solution
+  double t_m = (-(x0*c1 - y0*c2) - TMath::Sqrt(delta))/(c1*c1 + c2*c2);//- solution
 
   //calculate the values of the intersection points (x,y,z)
   if (t_p >= 0) {
@@ -49,7 +49,6 @@ double *hit_point(double x0, double y0, double z0, double theta, double phi, dou
     hit[2] = z0 + c3*t_m;
   }
 
-  //returns the pointer to the first element of the array hit which contains the values x, y and z
   return hit;
 }
 
@@ -58,7 +57,7 @@ void detect(int index, Hit* vtx, Layer* L, Particle &part, TClonesArray cross, b
   double *hit_buffer;
   bool b_cross=false;
 
-  hit_buffer=hit_point(vtx->GetX(),vtx->GetY(),vtx->GetZ(),part.GetTheta(),part.GetPhi(),L->GetRadius());
+  hit_buffer=hit_point(vtx->GetX(),vtx->GetY(),vtx->GetZ(),part.GetTheta(),part.GetPhi(),L->GetRadius());//evaluate hit point coordinates
 
   if(*(hit_buffer+2) >= -(L->GetWidth()/2.) && *(hit_buffer+2) <= (L->GetWidth()/2.)) {
 
