e38dd815f2aadb7cf1825d6ece3c46b1c8d61140
Mattia Ivaldi <mattia.ivaldi@gmail.com>
2018-12-02 20:51:19 +0100

    edit

--- spit_perform.C
+++ spit_perform.C
@@ -5,15 +5,19 @@ void spit_perform(){
   TStopwatch timer;
   timer.Start(true);//start cpu monitor
 
-  double z_custom[9]={-13.,-9.,-4.,-2.,0.,2.,4.,9.,13.};
-  double mult_custom[11]={2,5,10,15,20,25,30,35,40,45,50};
-  double resoz[9], resom[11], effm[11];
+  TString dirplot=TString("/Users/mattiaivaldi/GitHub/TRACKS/")+"tracksplot/";
+
+  const int kTest=11;
+
+  double z_custom[kTest]={-13.,-10.,-7.,-4.,-2.,0.,2.,4.,7.,10.,13.};
+  double mult_custom[kTest]={2,5,10,15,20,25,30,35,40,45,50};
+  double resoz[kTest], resom[kTest], effm[kTest];
 
   TString z, m, exec;
 
   printf("+++ START resolution performances +++\n\n");
 
-  for(int i=0; i<9;i++){
+  for(int i=0; i<kTest;i++){
     z=Form("%f",z_custom[i]);
     exec="tracks_gen(0,0,1,1,15,100000,"+z+",20)";
     gROOT->ProcessLine(exec);
@@ -23,7 +27,7 @@ void spit_perform(){
 
   printf("+++ START efficiency performances +++\n\n");
 
-  for(int i=0; i<11;i++){
+  for(int i=0; i<kTest;i++){
     m=Form("%f",mult_custom[i]);
     exec="tracks_gen(0,0,1,1,15,100000,0,"+m+")";
     gROOT->ProcessLine(exec);
@@ -32,35 +36,37 @@ void spit_perform(){
     effm[i]=perform.eff;
   }
 
-  TCanvas *c_reso=new TCanvas("c_reso","c_reso",600,400);
-  c_reso->Divide(2,1);
-  c_reso->cd(1);
-  TGraph *p_resoz=new TGraph(9,z_custom,resoz);
+  TCanvas *c_perform=new TCanvas("c_perform","c_perform",600,400);
+  c_perform->Divide(2,2);
+  c_perform->cd(1);
+  TGraph *p_resoz=new TGraph(kTest,z_custom,resoz);
   p_resoz->SetTitle("TRACKS performances - resolution vs Z_{gen};z_{gen} [cm];RMS [cm]");
-  graphstyler(*p_resoz,2);
+  graphstyler(*p_resoz,4);
+  p_resoz->GetYaxis()->SetTitleOffset(1.1);
   p_resoz->SetMarkerStyle(20);
-  p_resoz->SetMarkerSize(0.6);
+  p_resoz->SetMarkerSize(0.4);
   p_resoz->SetMarkerColor(1);
   p_resoz->Draw("AP");
-  c_reso->cd(2);
-  TGraph *p_resom=new TGraph(11,mult_custom,resom);
+  c_perform->cd(2);
+  TGraph *p_resom=new TGraph(kTest,mult_custom,resom);
   p_resom->SetTitle("TRACKS performances - resolution vs multiplicity;multiplicity;RMS [cm]");
-  graphstyler(*p_resom,2);
+  graphstyler(*p_resom,4);
+  p_resom->GetYaxis()->SetTitleOffset(1.1);
   p_resom->SetMarkerStyle(20);
-  p_resom->SetMarkerSize(0.6);
+  p_resom->SetMarkerSize(0.4);
   p_resom->SetMarkerColor(1);
   p_resom->Draw("AP");
-
-  TCanvas *c_eff=new TCanvas("c_eff","c_eff",600,400);
-  c_eff->Divide(2,1);
-  c_eff->cd(1);
-  TGraph *p_effm=new TGraph(11,mult_custom,effm);
+  c_perform->cd(3);
+  TGraph *p_effm=new TGraph(kTest,mult_custom,effm);
   p_effm->SetTitle("TRACKS performances - #varepsilon vs multiplicity;multiplicity;#varepsilon");
-  graphstyler(*p_effm,2);
+  graphstyler(*p_effm,4);
+  p_effm->GetYaxis()->SetTitleOffset(0.5);
+  p_effm->GetYaxis()->SetTitleSize(0.07);
   p_effm->SetMarkerStyle(20);
-  p_effm->SetMarkerSize(0.6);
+  p_effm->SetMarkerSize(0.4);
   p_effm->SetMarkerColor(1);
   p_effm->Draw("AP");
+  c_perform->SaveAs(dirplot+"c_perform.eps");
 
   //cpu info
   timer.Stop();
